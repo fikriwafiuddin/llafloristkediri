aa40e781537d894c7d12e31f8efaa8a1173175db
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-11-17 10:10:57 +0700

    setup model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ["customer_name", "whatsapp_number", "address", "budget", "status", "is_paid", "shipping_method", "schedule", "notes"];
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ["category_id", "name", "price", "description", "image"];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
