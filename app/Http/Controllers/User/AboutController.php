2bb3751e67ce8ee65a4082908133edb7f24043dd
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-11-24 09:21:20 +0700

    feat: add about page for user

--- app/Http/Controllers/User/AboutController.php
+++ app/Http/Controllers/User/AboutController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class AboutController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        
+        return view('user.about');
     }
 }
