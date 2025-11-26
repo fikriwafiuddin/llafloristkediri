712121bb38dcd583d0b1101ffba0a463a5e6d691
naylasalsa402 <naylasalsa402@gmail.com>
2025-11-26 13:05:24 +0700

    fix: memperbaiki seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,35 +15,35 @@ class ProductSeeder extends Seeder
     {
         $products = [
             [
-                'category_id' => 4,
+                'category_id' => 2,
                 'name' => 'Papan Bunga Ucapan Selamat',
                 'price' => 550000,
                 'description' => 'Papan bunga warna cerah dengan tulisan selamat.',
                 'image' => 'products\/wRzGcTu2gVWWnasMnYxFdQtaYmQuRRHRNQKDmNEf.jpg'
             ],
             [
-                'category_id' => 3,
+                'category_id' => 1,
                 'name' => 'Buket Campuran Pastel',
                 'price' => 270000,
                 'description' => 'Campuran bunga warna pastel seperti pink, peach, dan putih yang menghadirkan kesan lembut.',
                 'image' => 'products\/2PQzs0sV7iet8Yp7VyuRd9M5OBXDNcVbCytkX7CR.jpg'
             ],
             [
-                'category_id' => 6,
+                'category_id' => 3,
                 'name' => 'Bunga Meja Lily Putih',
                 'price' => 280000,
                 'description' => 'Arragement bunga lily putih dalam vas kaca modern, cocok untuk dekorasi ruangan.',
                 'image' => 'products\/4ikv9Egb9S1uOq0VMaQehGJK4zyKk9eWB7Vm2lOU.jpg'
             ],
             [
-                'category_id' => 3,
+                'category_id' => 1,
                 'name' => 'Bouquet Tulip Putih Elegant',
                 'price' => 320000,
                 'description' => 'Bouquet berisi 10 tulip putih impor dengan tampilan sederhana dan mewah.',
                 'image' => 'products\/zSF54wH3Nb9wmDCxGGyNfMpSHDyuRg4SVVIvTlIh.webp'
             ],
             [
-                'category_id' => 3,
+                'category_id' => 1,
                 'name' => 'Bouquet Mawar Merah Premium',
                 'price' => 350000,
                 'description' => 'Bouquet elegan berisi 12 mawar merah premium yang dibungkus dengan kertas coklat klasik dan pita satin.',
