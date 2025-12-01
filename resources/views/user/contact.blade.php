0183a9fd19f4d2868e45dfbce3752beb35c83f86
naylasalsa402 <naylasalsa402@gmail.com>
2025-12-01 09:47:51 +0700

    feat: add nomer contact

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -39,7 +39,7 @@
                     </div>
                     <div>
                         <h3 class="font-bold text-gray-700">WhatsApp / Telepon</h3>
-                        <p class="text-gray-600 text-sm mt-1">08560693276</p>
+                        <p class="text-gray-600 text-sm mt-1">08560693764</p>
                         <a href="https://wa.me/6281234567890" target="_blank" class="text-pink-500 text-sm font-semibold hover:underline mt-1 block">
                             Chat Admin Sekarang &rarr;
                         </a>
