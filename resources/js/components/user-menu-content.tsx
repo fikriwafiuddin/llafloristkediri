a1f816fa6d4c14449574c92b95b5920697277bcf
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-12-05 22:37:07 +0700

    fix: fix after logout

--- resources/js/components/user-menu-content.tsx
+++ resources/js/components/user-menu-content.tsx
@@ -22,6 +22,7 @@ export function UserMenuContent({ user }: UserMenuContentProps) {
     const handleLogout = () => {
         cleanup();
         router.flushAll();
+        window.location.href = '/';
     };
 
     return (
