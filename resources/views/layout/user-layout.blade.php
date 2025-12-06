e873dd3f23c43c36a48444bcc1b54eea0f6909c2
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-12-06 22:46:41 +0700

    feat: change favicon and title

--- resources/views/layout/user-layout.blade.php
+++ resources/views/layout/user-layout.blade.php
@@ -6,7 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <title>@yield('title') llafloristkediri</title>
+
+    <title>@yield('title') {{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="icon" href="{{ asset('logo.png') }}" sizes="any">
+    <link rel="icon" href="{{ asset('logo.png') }}" type="image/svg+xml">
 </head>
 <body>
     <x-navbar/>
